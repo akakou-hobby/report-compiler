1baadb1a2bc8889c1b1a31ce1992d52d805171c7
akakou <akama@keio.jp>
2020-10-17 20:07:06 +0900

    [add] Receive post request

--- app/serv.py
+++ app/serv.py
@@ -1,1 +1,16 @@
-print('hello')
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route("/hello", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form["name"]
+        return f"Hello, {name}"
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    # サーバ立ち上げ
+    app.run(
+        host="0.0.0.0",
+        port=5000)
